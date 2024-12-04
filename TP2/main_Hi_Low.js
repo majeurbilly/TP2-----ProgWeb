0ab41b72af0b38dba5199bada09a7205f236a92f
majeurbilly <billy.halle.bh@gmail.com>
2024-12-03 23:55:43 -0500

    exercice 1:  html✅  css✅  js✅  exercice 2:  html✅  css❌  js❌

--- TP2/main_Hi_Low.js
+++ TP2/main_Hi_Low.js
@@ -2,7 +2,7 @@ const numberInput = document.getElementById("numberInput");
 const messageErreur  = document.getElementById("messageErreur");
 const formulaireInscription = document.getElementById("formulaireInscription");
 formulaireInscription.onsubmit = GetNumber;
-const rndNumber =  Math.floor(Math.random() * 100 + 1);
+const rndNumber =  Math.floor(Math.random() * 100) + 1;
 let counter = 0;
 
 function GetNumber(evenement) {
@@ -16,16 +16,18 @@ function GetNumber(evenement) {
         counter++;
         evenement.preventDefault();
     } else if (rndNumber > userNumber) {
-        messageErreur.textContent = "Le nombre à deviner est plus élevé que " + userNumber + " 👇";
+        messageErreur.textContent = "Le nombre à deviner est plus élevé que " + userNumber + " 👆";
         numberInput.classList.add("border-danger");
         messageErreur.classList.remove('d-none');
         counter++;
         evenement.preventDefault();
     } else {
-        // Hide the input and show success message
+        messageErreur.classList.add('d-none');
         formulaireInscription.innerHTML = `
-            <p>Bravo! Vous avez gagné en ${counter} coups.</p>
-            <button id="replayButton">Jouer à nouveau</button>
+            <div class="text-center">
+                <p>Bravo! Vous avez gagné en ${counter} coups.</p>
+                <button id="replayButton">Jouer à nouveau</button>
+            </div>
         `;
 
         // Add event listener to the replay button
